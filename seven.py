aafc95ab8db5e9bda58d7cdba9f8564cb7e01b07
rispri <risprirajangupta@gmail.com>
2026-02-26 23:03:05 +0530

    seventh day

---
+++ seven.py
@@ -0,0 +1,8 @@
+# Write a program to find the sum of two numbers.
+
+def sum(num1,num2):
+    print(f'The sum of {num1} and {num2} is {num1+num2}')
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+sum(num1,num2)
