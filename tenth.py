b5d60165e889358ef951f643ee81f703de417ae7
rispri <risprirajangupta@gmail.com>
2026-03-03 23:59:41 +0530

    eleven

--- tenth.py
+++ tenth.py
@@ -1,4 +1,5 @@
 # Write a program to check if the given number is palindrome or not.
+# Palindrome is a number which is same when read from left to right and right to left.
 
 def palindrome(number):
-    
+hi
